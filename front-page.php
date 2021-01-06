3942a96556b82cc421d35652796ed5a500e357a7
Ernesto Hinojosa <eh@ernesto.one>
2021-01-06 13:09:16 +0100

    update svg icons to the latest ones and add animation

--- front-page.php
+++ front-page.php
@@ -70,17 +70,26 @@
     </div>
     <div class="row">
       <div class="col-md-4">
-        <!-- the animation is triggered when the icon is visible on the screen, via the js -hook -->
+        <!-- the animation is triggered when the icon is visible on the screen, via the js-hook -->
         <div class="AboutCard__iconWrapper">
-          <svg class="AboutCard__icon js-AboutCard__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-            <g>
-              <path class="utils-fill" d="M37.35,17.2a2.69,2.69,0,0,1-.51,1.47l-6.17,8.5a19.46,19.46,0,0,1,6.66,13.5h4a19.46,19.46,0,0,0-6.66-13.5l6.17-8.5a2.69,2.69,0,0,0,.51-1.47c.11-3.06.55-5.59-5.35-8.53C33.33,7.33,28,4.5,28,4.5V6.61c1.42.74,2.92,1.52,4,2.06C37.9,11.61,37.46,14.14,37.35,17.2Z"></path>
-              <path class="utils-fill" d="M45,47.4l-1-5.07C43.64,41.25,42.48,41,41.33,41h-4c1.15,0,2.31.25,2.67,1.33l1,5.07A1.32,1.32,0,0,1,40,49a1.09,1.09,0,0,1-.26,0h4A1.09,1.09,0,0,0,44,49,1.32,1.32,0,0,0,45,47.4Z"></path>
-              <path class="utils-stroke" d="M14,41H10.25a2.69,2.69,0,0,0-2.39,1.47L2.1,54H61.9L56.14,42.47A2.69,2.69,0,0,0,53.75,41H48"></path>
-              <path class="utils-stroke" d="M1.33,53.67,2.16,57a2.67,2.67,0,0,0,2.59,2h54.5a2.67,2.67,0,0,0,2.59-2l.83-3.31"></path>
-              <path class="utils-stroke" d="M22.67,40.67a23.65,23.65,0,0,1,4-14.67,45.76,45.76,0,0,1,4-4H20c-1.79,0-2.11-1-2.67-2.67a2.7,2.7,0,0,1,1.34-2.66L28,10V4.5s5.33,2.83,8,4.17c5.9,2.94,5.46,5.47,5.35,8.53a2.69,2.69,0,0,1-.51,1.47l-6.17,8.5a19.46,19.46,0,0,1,6.66,13.5"></path>
-              <path class="utils-stroke" d="M45,47.4A1.32,1.32,0,0,1,44,49a1.09,1.09,0,0,1-.26,0H20.29A1.34,1.34,0,0,1,19,47.67a1.24,1.24,0,0,1,0-.27l1-5.07c.36-1.08,1.52-1.33,2.67-1.33H41.33c1.15,0,2.31.25,2.67,1.33Z"></path>
-              <path class="utils-stroke" d="M52.67,46.08h0a.67.67,0,0,1,.66.67h0a.67.67,0,0,1-.66.67h0a.67.67,0,0,1-.67-.67h0a.67.67,0,0,1,.67-.67"></path>
+          <svg class="AboutCard__icon js-AboutCard__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 81">
+            <g fill="none" fill-rule="evenodd">
+              <g>
+                <g>
+                  <g>
+                    <g fill="#FBF9F0" stroke="#3B012D" stroke-opacity=".04" stroke-width="2">
+                      <path d="M50 1c8.008 0 15.258 3.246 20.506 8.494C75.754 14.742 79 21.992 79 30h0v20c0 8.008-3.246 15.258-8.494 20.506C65.258 75.754 58.008 79 50 79h0-20c-8.008 0-15.258-3.246-20.506-8.494C4.246 65.258 1 58.008 1 50h0V30c0-8.008 3.246-15.258 8.494-20.506C14.742 4.246 21.992 1 30 1h0z" transform="translate(-160 -1343) translate(0 975.257) translate(160 367.956)"/>
+                    </g>
+                    <g>
+                      <g>
+                        <path class="utils-fill" fill-rule="nonzero" d="M1.281 38.561h43.437l-.31 1.244c-.111.445-.511.757-.97.757H2.562l-.113-.006c-.41-.047-.755-.343-.857-.751l-.311-1.244zM30 28.566l.164.003c.52.016.78.116.866.266l.007.015.743 3.716H14.22l.742-3.716-.013.032c.064-.193.362-.316 1.051-.316h14z" transform="translate(-160 -1343) translate(0 975.257) translate(160 367.956) translate(8 7) translate(9 11.5)"/>
+                        <path class="utils-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 27.562H6.236c-.757 0-1.45.429-1.788 1.106L0 37.562h46l-4.448-8.894c-.339-.677-1.03-1.106-1.788-1.106H35M0 37.562l.622 2.486c.223.89 1.023 1.514 1.94 1.514h40.876c.917 0 1.717-.624 1.94-1.514L46 37.562M16 27.562c-.237-3.897.818-7.763 3-11 .937-1.061 1.939-2.063 3-3h-8c-1.344 0-1.582-.72-2-2-.099-.807.295-1.595 1-2l7-5V.438s4 2.124 6 3.124c4.42 2.21 4.092 4.106 4.012 6.4-.014.398-.146.782-.38 1.104L25 17.438c2.957 2.568 4.759 6.215 5 10.124" transform="translate(-160 -1343) translate(0 975.257) translate(160 367.956) translate(8 7) translate(9 11.5)"/>
+                        <path class="utils-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M32.76 32.366c.06.294-.015.6-.205.833-.19.232-.475.367-.775.367H14.22c-.3 0-.585-.135-.775-.367-.19-.233-.265-.539-.205-.833l.76-3.8c.272-.816 1.14-1 2-1h14c.86 0 1.728.184 2 1l.76 3.8zM38.5 31.562h0c.276 0 .5.224.5.5h0c0 .276-.224.5-.5.5h0c-.276 0-.5-.224-.5-.5h0c0-.276.224-.5.5-.5" transform="translate(-160 -1343) translate(0 975.257) translate(160 367.956) translate(8 7) translate(9 11.5)"/>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
             </g>
           </svg>
         </div>
@@ -88,17 +97,27 @@
         <p class="AboutCard__subHeading">Har du brug for at få lagt den rigtige plan? Eller bare få ført den du har ud i livet? Vi kan hjælpe.</p>
       </div>
       <div class="col-md-4">
-        <div class="AboutCard__iconWrapper">
-          <svg class="AboutCard__icon  js-AboutCard__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-            <g>
-              <path class="utils-fill" d="M57.73,5a1.07,1.07,0,0,0-.25,0l-1.68.36c-.72,3.38-2.14,8.94-4,10.78L22.65,45.31l.93.93,1.58,1.57L35.54,37.42,54.83,18.15c2-2,3.5-8.44,4.15-11.65A1.4,1.4,0,0,0,59,6a1.29,1.29,0,0,0-1.25-1"></path>
-              <path class="utils-stroke" d="M16.21,38.87S44,11,45.84,9.16c2-2,8.43-3.51,11.64-4.15A1.25,1.25,0,0,1,59,6a1.06,1.06,0,0,1,0,.5c-.64,3.21-2.15,9.65-4.14,11.65L25.16,47.81Z"></path>
-              <path class="utils-stroke" d="M19.79,35.24l9,9"></path>
-              <path class="utils-stroke" d="M38.64,34.33A55.31,55.31,0,0,1,37.75,46a17.24,17.24,0,0,1-3.61,5.39l-6.4,6.39a1.27,1.27,0,0,1-2.16-1.05l1.24-10.61"></path>
-              <path class="utils-stroke" d="M29.65,25.34a55.82,55.82,0,0,0-11.76.81,17.32,17.32,0,0,0-5.32,3.68L6.19,36.21a1.27,1.27,0,0,0,0,1.8,1.29,1.29,0,0,0,1,.36l10.68-1.31"></path>
-              <path class="utils-stroke" d="M19.76,47.81c1.8,1.8,1,4.35-.89,6.3S10.13,58,6.59,59A1.28,1.28,0,0,1,5,58.07a1.38,1.38,0,0,1,0-.64c1-3.55,3-10.48,4.83-12.3,2-2,4.5-2.7,6.3-.9"></path>
+        <div class="AboutCard__iconWrapper">          
+          <svg class="AboutCard__icon js-AboutCard__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 81">
+            <g fill="none" fill-rule="evenodd">
+              <g>
+                <g>
+                  <g>
+                    <g>
+                      <g fill="#FBF9F0" stroke="#3B012D" stroke-opacity=".04" stroke-width="2">
+                        <path d="M50 1c8.008 0 15.258 3.246 20.506 8.494C75.754 14.742 79 21.992 79 30h0v20c0 8.008-3.246 15.258-8.494 20.506C65.258 75.754 58.008 79 50 79h0-20c-8.008 0-15.258-3.246-20.506-8.494C4.246 65.258 1 58.008 1 50h0V30c0-8.008 3.246-15.258 8.494-20.506C14.742 4.246 21.992 1 30 1h0z" transform="translate(-544 -1343) translate(0 975.257) translate(160 367.956) translate(384)"/>
+                      </g>
+                      <g>
+                        <path class="utils-fill" d="M27.996 27.99l-7.295 7.293-8.059.986 5.02-5.018.31-.304c.943-.893 2.015-1.634 3.18-2.197l.283-.132-.12.02c2.168-.384 4.362-.6 6.562-.647l.119-.001zM29.118 43.838l7.347-7.345-.002.084c-.054 1.915-.238 3.824-.55 5.716l-.121.696.042-.095c-.62 1.464-1.51 2.799-2.624 3.933l-5.03 5.022.938-8.011z" transform="translate(-544 -1343) translate(0 975.257) translate(160 367.956) translate(384) translate(8 7)"/>
+                        <path class="utils-stroke" d="M37.461 34.084c.092 3.083-.145 6.167-.706 9.2-.668 1.58-1.629 3.02-2.832 4.244l-5.034 5.026c-.3.302-.76.38-1.144.195-.385-.186-.608-.595-.558-1.02l.978-8.351M19.813 37.654s21.89-21.956 23.306-23.37c1.57-1.572 6.636-2.756 9.156-3.264.329-.066.668.036.905.273.237.237.34.576.273.905-.508 2.52-1.692 7.584-3.264 9.156L26.853 44.688l-7.04-7.034zM22.633 34.802L29.685 41.86M30.39 27.012c-3.098-.115-6.199.1-9.25.642-1.565.691-2.984 1.672-4.185 2.89l-5.02 5.018c-.302.302-.38.762-.192 1.146.188.384.598.606 1.022.554l8.4-1.028M22.611 44.688c1.416 1.414.826 3.418-.706 4.952-1.434 1.432-6.87 3.066-9.652 3.83-.342.09-.707-.009-.958-.259-.252-.25-.352-.614-.264-.957.756-2.79 2.374-8.242 3.8-9.678 1.554-1.552 3.538-2.12 4.952-.706" transform="translate(-544 -1343) translate(0 975.257) translate(160 367.956) translate(384) translate(8 7)"/>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
             </g>
-          </svg> 
+          </svg>
+
         </div>
         <h3 class="AboutCard__heading">Design & brugeroplevelse</h3>
         <p class="AboutCard__subHeading">Vi designer den digitale kunderejse, der giver dig det rigtige aftryk i markedet og som kan mærkes på bundlinjen</p>
@@ -106,25 +125,26 @@
       <div class="col-md-4">
         <div class="AboutCard__iconWrapper">
           <!-- the animation is triggered when the icon is visible on the screen, via the js-hook -->
-          <svg class="AboutCard__icon  js-AboutCard__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-            <g>
-              <path class="utils-fill" d="M52.79,16.2a9.77,9.77,0,0,1-2,6A7.74,7.74,0,0,1,55,23.86,9.73,9.73,0,0,0,49.26,8.77,9.69,9.69,0,0,1,52.79,16.2Z"></path>
-              <path class="utils-fill" d="M54.74,39.77a7.79,7.79,0,0,0,.81-15.45,7.8,7.8,0,0,1-4.81,13.45A9.73,9.73,0,0,1,43,53.48l-.33,0-.52,0a5.21,5.21,0,0,1-3.62,6.36,4,4,0,0,1-.59.1A5.21,5.21,0,0,0,41.67,62a5.31,5.31,0,0,0,.87-.16,5.21,5.21,0,0,0,3.6-6.44c.29,0,.57.08.87.08h.15s0,0,0-.08c.18,0,.36,0,.54.06a9.73,9.73,0,0,0,7.06-15.69Z"></path>
-              <path class="utils-stroke" d="M24.53,12.13a5.22,5.22,0,1,0-6.88-4.92,5.09,5.09,0,0,0,.21,1.39A8.27,8.27,0,0,0,17,8.51,9.74,9.74,0,0,0,9.26,24.23a7.79,7.79,0,0,0,0,15.54A9.73,9.73,0,0,0,17,55.48c.3,0,.58-.06.87-.08a5.22,5.22,0,1,0,6.67-3.53"></path>
-              <path class="utils-stroke" d="M39.47,12.13a5.22,5.22,0,1,1,6.88-4.92,5.09,5.09,0,0,1-.21,1.39A8.27,8.27,0,0,1,47,8.51a9.74,9.74,0,0,1,7.73,15.72,7.79,7.79,0,0,1,0,15.54A9.73,9.73,0,0,1,47,55.48c-.3,0-.58-.06-.87-.08a5.22,5.22,0,1,1-6.67-3.53"></path>
-              <path class="utils-stroke" d="M28.09,6.94a7.85,7.85,0,0,1,7.82,0"></path>
-              <path class="utils-stroke" d="M35.91,57.05a7.81,7.81,0,0,1-7.82,0"></path>
-              <path class="utils-stroke" d="M22,21H43V42H22Z"></path>
-              <path class="utils-stroke" d="M27,16v5"></path>
-              <path class="utils-stroke" d="M37,16v5"></path>
-              <path class="utils-stroke" d="M16,37h5"></path>
-              <path class="utils-stroke" d="M16,27h5"></path>
-              <path class="utils-stroke" d="M37,48V43"></path>
-              <path class="utils-stroke" d="M27,48V43"></path>
-              <path class="utils-stroke" d="M48,27H43"></path>
-              <path class="utils-stroke" d="M48,37H43"></path>
+          <svg class="AboutCard__icon  js-AboutCard__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 81">
+            <g fill="none" fill-rule="evenodd">
+              <g>
+                <g>
+                  <g>
+                    <g>
+                      <g fill="#FBF9F0" stroke="#3B012D" stroke-opacity=".04" stroke-width="2">
+                        <path d="M50 1c8.008 0 15.258 3.246 20.506 8.494C75.754 14.742 79 21.992 79 30h0v20c0 8.008-3.246 15.258-8.494 20.506C65.258 75.754 58.008 79 50 79h0-20c-8.008 0-15.258-3.246-20.506-8.494C4.246 65.258 1 58.008 1 50h0V30c0-8.008 3.246-15.258 8.494-20.506C14.742 4.246 21.992 1 30 1h0z" transform="translate(-928 -1343) translate(0 975.257) translate(160 367.956) translate(768)"/>
+                      </g>
+                      <g>
+                        <path class="utils-fill" fill-rule="nonzero" d="M38.991 25.018L38.991 39.018 24.991 39.018 24.991 25.018z" transform="translate(-928 -1343) translate(0 975.257) translate(160 367.956) translate(768) translate(8 7)"/>
+                        <path class="utils-stroke" d="M26.27 16.79c1.936-.652 3.07-2.66 2.626-4.656-.442-1.996-2.318-3.336-4.35-3.109-2.03.227-3.564 1.95-3.555 3.994.006.36.06.72.162 1.066-.222-.02-.436-.066-.662-.066-2.856-.014-5.47 1.604-6.732 4.166-1.262 2.563-.95 5.622.802 7.878C11.431 26.277 9 28.88 9 32.019c0 3.138 2.43 5.74 5.561 5.956-1.753 2.255-2.064 5.314-.802 7.877 1.262 2.563 3.876 4.18 6.732 4.167.226 0 .44-.046.662-.066-.594 2.102.609 4.292 2.702 4.92 2.093.626 4.301-.542 4.96-2.625.66-2.083-.473-4.31-2.546-5.001M37.713 16.79c-1.936-.652-3.07-2.66-2.627-4.656.443-1.996 2.32-3.336 4.35-3.109 2.032.227 3.565 1.95 3.555 3.994-.005.36-.06.72-.162 1.066.222-.02.436-.066.662-.066 2.857-.014 5.471 1.604 6.733 4.166 1.262 2.563.95 5.622-.803 7.878 3.132.214 5.562 2.817 5.562 5.956 0 3.138-2.43 5.74-5.562 5.956 1.753 2.255 2.065 5.314.803 7.877-1.262 2.563-3.876 4.18-6.733 4.167-.226 0-.44-.046-.662-.066.595 2.102-.608 4.292-2.7 4.92-2.094.626-4.303-.542-4.962-2.625-.66-2.083.474-4.31 2.546-5.001M28.991 12.819c1.857-1.072 4.144-1.072 6 0M34.991 51.219c-1.856 1.071-4.143 1.071-6 0" transform="translate(-928 -1343) translate(0 975.257) translate(160 367.956) translate(768) translate(8 7)"/>
+                        <path class="utils-stroke" d="M23.991 24.019L39.991 24.019 39.991 40.019 23.991 40.019zM27.991 20.019L27.991 24.019M35.991 20.019L35.991 24.019M19.991 36.019L23.991 36.019M19.991 28.019L23.991 28.019M35.991 44.019L35.991 40.019M27.991 44.019L27.991 40.019M43.991 28.019L39.991 28.019M43.991 36.019L39.991 36.019" transform="translate(-928 -1343) translate(0 975.257) translate(160 367.956) translate(768) translate(8 7)"/>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
             </g>
-          </svg>    
+          </svg>
         </div>
         <h3 class="AboutCard__heading">Martech</h3>
         <p class="AboutCard__subHeading">Få en Martech løsning der sikrer at i får mest muligt ud af Jeres kundedata, og kan fungere som omdrejningspunkt for Jeres kommunikation</p>
@@ -207,14 +227,14 @@
 <!-- /Quote -->
 
 <!-- BlogPromo -->
-  <div class="BlogPromo container">
+<!--   <div class="BlogPromo container">
     <div class="row justify-content-center">
       <div class="col-md-8">
         <h2 class="BlogPromo__heading">Viden & Indsigt</h2>
         <p  class="BlogPromo__subHeading">Free directories - directories are perfect for customers that are searching for a particular topic. What’s great about them is that you only have to post once and they are good for long periods of time.</p>
       </div>
-    </div><!-- /row justify-content-center -->
-  </div><!-- /container -->
+    </div>
+  </div> -->
 <!-- /BlogPromo -->
 
 <?php get_footer(); ?>
