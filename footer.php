5812908b767ede35f9a59b4dc8e6c46abf6aeec8
Ernesto Hinojosa <eh@ernesto.one>
2021-01-08 09:52:56 +0100

    add footer markup

--- footer.php
+++ footer.php
@@ -1,4 +1,45 @@
 
-<?php wp_footer(); ?>
+<!-- Footer -->
+  <div class="container-fluid p-0">
+    <div class="container">
+
+      <div class="row justify-content-between">
+        <div class="col-md-4 order-md-2">
+          <h3>Tilmeld nyhedsbrev</h3>
+          <p>Hold dig opdateret om Marketing Automation. Du kan afmelde dig til hver en tid via nyhedsbrevet.</p>
+          <span>Email Adresse</span><button type="button" class="ButtonSolid--medium">Button</button>
+        </div>
+        <div class="col-md-4 order-md-1">
+          <p>31 70 01 01<br>hello@autobahncph.dk</p>
+          <ul>
+            <li>Autobahn ApS</li>
+            <li>Vesterbrogade 37, 1. th</li>
+            <li>1620 København V</li>
+            <li>Danmark</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="row justify-content-between">
+        <div class="col-auto order-md-2">
+          <div>
+            <div>i</div>
+            <div>i</div>
+            <div>i</div>
+            <div>i</div>
+          </div>
+        </div>
+        <div class="col-md-4 order-md-1">
+          <p>&copy; <?php echo date("Y"); ?> - Autobahn ApS</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+<!-- /Footer -->
+
+<!-- wp_footer() -->
+  <?php wp_footer(); ?>
+<!-- /wp_footer() -->
 </body>
 </html>
