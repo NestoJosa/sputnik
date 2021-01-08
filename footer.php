43111189f31d8559f9ac04658866feb0baa8d65e
Ernesto Hinojosa <eh@ernesto.one>
2021-01-08 14:03:09 +0100

    finish newsletter signup

--- footer.php
+++ footer.php
@@ -1,45 +1,52 @@
-
 <!-- Footer -->
-  <div class="container-fluid p-0">
-    <div class="container">
+  <footer>
+    <div class="FooterWrapper container-fluid p-0">
+      <div class="container">
 
-      <div class="row justify-content-between">
-        <div class="col-md-4 order-md-2">
-          <h3>Tilmeld nyhedsbrev</h3>
-          <p>Hold dig opdateret om Marketing Automation. Du kan afmelde dig til hver en tid via nyhedsbrevet.</p>
-          <span>Email Adresse</span><button type="button" class="ButtonSolid--medium">Button</button>
-        </div>
-        <div class="col-md-4 order-md-1">
-          <p>31 70 01 01<br>hello@autobahncph.dk</p>
-          <ul>
-            <li>Autobahn ApS</li>
-            <li>Vesterbrogade 37, 1. th</li>
-            <li>1620 København V</li>
-            <li>Danmark</li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="row justify-content-between">
-        <div class="col-auto order-md-2">
-          <div>
-            <div>i</div>
-            <div>i</div>
-            <div>i</div>
-            <div>i</div>
+        <div class="row justify-content-between">
+          <div class="NewsLetterSignup col-md-4 order-md-2">
+            <h3 class="NewsLetterSignup__title">Tilmeld nyhedsbrev</h3>
+            <p class="NewsLetterSignup__subTitle">Hold dig opdateret om Marketing Automation. Du kan afmelde dig til hver en tid via nyhedsbrevet.</p>
+            <div class="NewsLetterSignup__buttonsWrapper">
+              <div class="NewsLetterSignup__emailInput">
+                <span>Email Adresse</span>
+              </div>
+              <button class="ButtonSolid--medium" type="button">Tilmeld nu</button>
+            </div>
+          </div>
+          <div class="col-md-4 order-md-1">
+            <p>31 70 01 01<br>hello@autobahncph.dk</p>
+            <ul>
+              <li>Autobahn ApS</li>
+              <li>Vesterbrogade 37, 1. th</li>
+              <li>1620 København V</li>
+              <li>Danmark</li>
+            </ul>
           </div>
         </div>
-        <div class="col-md-4 order-md-1">
-          <p>&copy; <?php echo date("Y"); ?> - Autobahn ApS</p>
+
+        <div class="row justify-content-between">
+          <div class="col-auto order-md-2">
+            <div>
+              <div>i</div>
+              <div>i</div>
+              <div>i</div>
+              <div>i</div>
+            </div>
+          </div>
+          <div class="col-md-4 order-md-1">
+            <p>&copy; <?php echo date("Y"); ?> - Autobahn ApS</p>
+          </div>
         </div>
+
       </div>
-
     </div>
-  </div>
-<!-- /Footer -->
 
-<!-- wp_footer() -->
-  <?php wp_footer(); ?>
-<!-- /wp_footer() -->
+    <!-- wp_footer() -->
+      <?php wp_footer(); ?>
+    <!-- /wp_footer() -->
+
+  </footer>
+<!-- /Footer -->
 </body>
 </html>
