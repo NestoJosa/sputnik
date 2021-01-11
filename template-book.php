6d4ee6030a32ab838f543aec600959394f14571c
Ernesto Hinojosa <eh@ernesto.one>
2021-01-11 13:12:20 +0100

    add new page templates

---
+++ template-book.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Template Name:  Book Template
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+
+  <h1><?php the_title(); ?></h1>
+
+  <!-- Include section-content -->
+  <?php get_template_part('includes/section', 'content');  ?>
+  <!-- /Include section-content -->
+
+</div>
+
+
+
+<?php get_footer(); ?>
