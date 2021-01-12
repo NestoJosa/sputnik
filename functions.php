1b45e17d357648be8c66eded97c27c996af16227
Ernesto Hinojosa <eh@ernesto.one>
2021-01-12 13:30:37 +0100

    add the correct bootstrap version in order to make the bootstrap navbar work

--- functions.php
+++ functions.php
@@ -2,33 +2,35 @@
 
   // Load Stylesheets
   function load_css() {
+    
     wp_register_style('adobe-fonts', 'https://use.typekit.net/kbs0hyb.css', array(), null, 'all' );
     wp_enqueue_style('adobe-fonts');
-
-    /* Note: bootstrap styles are inside the following bundle */
-    wp_register_style('bundle', get_template_directory_uri() . '/dist/css/bundle.css', array(), '1', 'all' );
-    wp_enqueue_style('bundle');
+        
+    wp_register_style('bootstrap-styles', get_template_directory_uri() . '/dist/css/bundle.css', array(), '1.0', 'all' );
+    wp_enqueue_style('bootstrap-styles'); 
+   
   }
-
   add_action('wp_enqueue_scripts', 'load_css');
 
+  
   // Load Javascript
   function load_js() {
-
+        
+    wp_register_script('bootstrap-scripts', get_template_directory_uri() . '/dist/bootstrap-4.0.0/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true );
+    wp_enqueue_script('bootstrap-scripts');
     
-    wp_register_script('popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js"', false, '2.6.0', true);
-    wp_enqueue_script('popper');    
-    
-    wp_register_script('bootstrap', get_template_directory_uri() . '/dist/bootstrap/dist/js/bootstrap.min.js', array('jquery'), null, true );
-    wp_enqueue_script('bootstrap');
-    
-    wp_enqueue_script( 'bundle', get_template_directory_uri() . '/dist/js/bundle.js', array(), null, true );
+        
+    wp_enqueue_script( 'bundle', get_template_directory_uri() . '/dist/js/bundle.js', array(), null, true ); 
+   
   }
 
   add_action('wp_enqueue_scripts', 'load_js');
-
   
-  // Theme Options
+  /* 
+  
+    Theme Options
+  
+  */
 
   // This theme uses post thumbnails
   add_theme_support( 'post-thumbnails' );
@@ -41,35 +43,16 @@
     
     array(
       'top-menu' => 'Top Menu Location',
-      'mobile-menu' => 'Mobile Menu Location',
     )
 
   );
 
   /**
-   * Register Custom Navigation Walker
+   * Register Custom Navigation Walker - required to change the default nav markup so that we can use bootstraps navbar
    */
   function register_navwalker(){
     require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
   }
   add_action( 'after_setup_theme', 'register_navwalker' );
-
-  
-  function my_init() {  
-    if (!is_admin()) {  
-      wp_deregister_script('jquery');  
-      wp_deregister_script('jquery-migrate');  
-
-      // Load a copy of jQuery from the Google API CDN  
-      // The last parameter set to TRUE states that it should be loaded  
-      // in the footer.  
-      wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.min.js', false, '3.5.1', true);  
-      wp_register_script('jquery-migrate', 'https://code.jquery.com/jquery-migrate-3.3.2.min.js"', false, '3.3.2', true);  
-
-      wp_enqueue_script('jquery');  
-      wp_enqueue_script('jquery-migrate');  
-    }  
-  }  
-  add_action('init', 'my_init');  
 
 ?>
