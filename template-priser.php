c18211175dbd15e5a668180b4eae4d602e67bb06
Ernesto Hinojosa <eh@ernesto.one>
2021-01-13 15:52:04 +0100

    add table data

--- template-priser.php
+++ template-priser.php
@@ -37,12 +37,21 @@ Template Name:  Priser Template
   </div>
 <!-- /HeadingBlock -->
 
-<!-- SubCards -->
-  <div class="container">
+<!-- table -->
+  <?php 
 
-    <!-- table -->
+    // icons for the table
+
+    $icon_check_box = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"> <g fill="none" fill-rule="evenodd"> <g> <g> <g transform="translate(-564 -1381) translate(160 1369) translate(404 12)"> <rect width="20" height="20" x="2" y="2" fill="#FF582D" rx="6"/> <g fill="#FBF9F0" transform="rotate(-45 18.45 1.672)"> <rect width="13" height="2" x=".793" y="4.55" rx="1"/> <rect width="2" height="6" x=".636" y=".464" rx="1"/> </g> </g> </g> </g> </g> </svg>';
+    
+    $icon_dash = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"> <g fill="none" fill-rule="evenodd"> <g fill="#FF582D"> <g> <g transform="translate(-564 -1429) translate(160 1417) translate(404 12)"> <rect width="8" height="2" x="8" y="11" rx="1"/> </g> </g> </g> </g></svg>';
+
+  ?>
+
+  <div class="container">
     <div class="row">
       <div class="col">
+
         <div class="SubTable">
 
             <div class="SubTable__lines"></div>
@@ -77,21 +86,66 @@ Template Name:  Priser Template
               <p class="SubTable__subName">Growth</p>        
               <p class="SubTable__subNameDesc">Simple Lead Journey</p>        
             </div>
+
+            <!-- Growth Data -->
+            <span class="SubTable__growthData SubTable__growthData-1">2000</span>
+            <span class="SubTable__growthData SubTable__growthData-2">Simple</span>
+            <span class="SubTable__growthData SubTable__growthData-3">2</span>
+            <span class="SubTable__growthData SubTable__growthData-4">15</span>
+            <span class="SubTable__growthData SubTable__growthData-5">Simple</span>
+            <span class="SubTable__growthData SubTable__growthData-6"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__growthData SubTable__growthData-7"><?php echo $icon_dash ?></span>
+            <span class="SubTable__growthData SubTable__growthData-8"><?php echo $icon_dash ?></span>
+            <span class="SubTable__growthData SubTable__growthData-9"><?php echo $icon_dash ?></span>
+            <span class="SubTable__growthData SubTable__growthData-10"><?php echo $icon_dash ?></span>
+            <span class="SubTable__growthData SubTable__growthData-11"><?php echo $icon_dash ?></span>
+            <span class="SubTable__growthData SubTable__growthData-12"><?php echo $icon_dash ?></span>
+            <!-- /Growth Data -->
+
             <div class="SubTable__nameWrapper SubTable__nameWrapper--scale">
               <p class="SubTable__subName">Scale</p>        
               <p class="SubTable__subNameDesc">Advanced Lead Journey</p>        
             </div>
+
+            <!-- Scale Data -->
+            <span class="SubTable__scaleData SubTable__scaleData-1">5000</span>
+            <span class="SubTable__scaleData SubTable__scaleData-2">Advanced</span>
+            <span class="SubTable__scaleData SubTable__scaleData-3">5</span>
+            <span class="SubTable__scaleData SubTable__scaleData-4">25</span>
+            <span class="SubTable__scaleData SubTable__scaleData-5">Advanced</span>
+            <span class="SubTable__scaleData SubTable__scaleData-6"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__scaleData SubTable__scaleData-7"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__scaleData SubTable__scaleData-8"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__scaleData SubTable__scaleData-9"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__scaleData SubTable__scaleData-10"><?php echo $icon_dash ?></span>
+            <span class="SubTable__scaleData SubTable__scaleData-11"><?php echo $icon_dash ?></span>
+            <span class="SubTable__scaleData SubTable__scaleData-12"><?php echo $icon_dash ?></span>
+            <!-- /Scale Data -->
+
             <div class="SubTable__nameWrapper SubTable__nameWrapper--enterprise">
               <p class="SubTable__subName">Enterprise</p>        
               <p class="SubTable__subNameDesc">Customer Journey</p>        
             </div>
 
+            <!-- Enterprise Data -->
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-1">10.000</span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-2">Advanced</span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-3">10</span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-4">50</span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-5">Advanced</span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-6"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-7"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-8"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-9"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-10"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-11"><?php echo $icon_check_box ?></span>
+            <span class="SubTable__enterpriseData SubTable__enterpriseData-12"><?php echo $icon_check_box ?></span>
+            <!-- /Enterprise Data -->
 
+        </div><!-- /SubTable -->
 
-        </div>
-      </div>
-    </div>
-    <!-- /table -->
+      </div><!-- /col -->
+    </div><!-- /row -->
 
     <div class="row justify-content-center">
       <div class="col-auto">
@@ -99,10 +153,10 @@ Template Name:  Priser Template
       </div>
     </div>
 
-  </div>
+  </div><!-- /container -->
 
 
-<!-- /SubCards -->
+<!-- /table -->
 
 
 
