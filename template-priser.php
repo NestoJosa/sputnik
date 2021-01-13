f67f4c82fd39f2167e3a92e1a687f989b7b6eeb3
Ernesto Hinojosa <eh@ernesto.one>
2021-01-13 08:29:34 +0100

    add content to template

--- template-priser.php
+++ template-priser.php
@@ -8,15 +8,49 @@ Template Name:  Priser Template
 
 <?php get_header(); ?>
 
-<div class="container">
+<!-- Hero -->
+<div class="Hero container-fluid p-0">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="Hero__image col-md-6 order-md-2">
+          <img src="<?php echo get_template_directory_uri();?>/dist/img/man-with-computer--large.png" alt="Man using computer">
+        </div>
+        <div class="col-md-6 order-md-1">
+          <h1 class="Hero__heading">Marketing Automation på abonnement</h1>
+          <p class="Hero__subHeading">Vi tilbyder Marketing Automation på abonnement så du kan få alle fordelen til en fast lav månedlig pris. Du slipper for alt besværet - og sparer samtidig penge.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- /Hero -->
 
-  <h1><?php the_title(); ?></h1>
+<!-- HeadingBlock -->
+<div class="HeadingBlock container">
+    <div class="row justify-content-center">
 
-  <!-- Include section-content -->
-  <?php get_template_part('includes/section', 'content');  ?>
-  <!-- /Include section-content -->
+      <div class="col-md-8">
+        <h2 class="HeadingBlock__heading">Alle fordelene. Uden alt besværet!</h2>
+        <p  class="HeadingBlock__subHeading">Free directories: directories are perfect for customers that are searching for a particular topic. What’s great about them is that you only have to post once and they are good for long periods of time. When you don’t have to resubmit your information every week or every month.</p>
+      </div>
 
-</div>
+    </div>   
+  </div>
+<!-- /HeadingBlock -->
+
+<!-- SubCards -->
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="SubTable">
+
+          I am table
+
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- /SubCards -->
+
 
 
 
