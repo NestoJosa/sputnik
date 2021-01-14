ff5dd2915eeb6a26b73acf26790f38d13a64565e
Ernesto Hinojosa <eh@ernesto.one>
2021-01-14 14:35:38 +0100

    finish price page

--- template-priser.php
+++ template-priser.php
@@ -9,11 +9,11 @@ Template Name:  Priser Template
 <?php get_header(); ?>
 
 <!-- Hero -->
-<div class="Hero container-fluid p-0">
+<div class="Hero Hero--onPricePage container-fluid p-0">
     <div class="container">
       <div class="row align-items-center">
         <div class="Hero__image col-md-6 order-md-2">
-          <img src="<?php echo get_template_directory_uri();?>/dist/img/man-with-computer--large.png" alt="Man using computer">
+          <img src="<?php echo get_template_directory_uri();?>/dist/img/man-with-computer-circle-bg--large.png" alt="Man using computer">
         </div>
         <div class="col-md-6 order-md-1">
           <h1 class="Hero__heading">Marketing Automation på abonnement</h1>
@@ -29,8 +29,9 @@ Template Name:  Priser Template
     <div class="row justify-content-center">
 
       <div class="col-md-8">
+        <p class="HeadingBlock__preHeading">Fordele</p>
         <h2 class="HeadingBlock__heading">Alle fordelene. Uden alt besværet!</h2>
-        <p  class="HeadingBlock__subHeading">Free directories: directories are perfect for customers that are searching for a particular topic. What’s great about them is that you only have to post once and they are good for long periods of time. When you don’t have to resubmit your information every week or every month.</p>
+        <p class="HeadingBlock__subHeading">Free directories: directories are perfect for customers that are searching for a particular topic. What’s great about them is that you only have to post once and they are good for long periods of time. When you don’t have to resubmit your information every week or every month.</p>
       </div>
 
     </div>   
