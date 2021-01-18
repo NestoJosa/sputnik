8e8655e668efac0ed643f8dc1457856aab044a1c
Ernesto Hinojosa <eh@ernesto.one>
2021-01-18 19:34:05 +0100

    finish styling the footer-newsletter-signup

---
+++ template-thank-you.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+
+  Template Name: Thank you template
+
+*/
+
+?>
+
+<?php get_header() ?>
+
+<!-- PageWrapper -->
+<div class="PageWrapper">
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <p>Tak for din tilmelding!</p>
+      </div>
+    </div>
+  </div>
+
+</div><!-- /PageWrapper -->
+
+<?php get_footer() ?>
