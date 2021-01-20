68df8df58ccb5d7598fb1b2fded30671c22dff27
Ernesto Hinojosa <eh@ernesto.one>
2021-01-20 11:41:04 +0100

    fix thank you page

--- template-thank-you.php
+++ template-thank-you.php
@@ -11,12 +11,12 @@
 <?php get_header() ?>
 
 <!-- PageWrapper -->
-<div class="PageWrapper">
+<div class="PageWrapper PageWrapper--onThankYouPage">
 
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <p>Tak for din tilmelding!</p>
+        <h1>Tak for din tilmelding!</h1>
       </div>
     </div>
   </div>
