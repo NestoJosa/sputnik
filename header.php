332e149bd81972d2f704cc57daeba6bab068e1f7
Ernesto Hinojosa <eh@ernesto.one>
2021-02-01 20:14:28 +0100

    update functions.php
    - add dynamic charset
    - add favicon

--- header.php
+++ header.php
@@ -2,12 +2,13 @@
 <html lang="en">
 <head>
 
-  <meta charset="utf-8">
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <meta name="format-detection" content="telephone=no">
 
-  <title></title>
+  <title><?php echo wp_title(); ?></title>
+
+  <link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri();?>/dist/img/favicon.png" />
 
   <?php wp_head(); ?>
 
