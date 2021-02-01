f264f5ac5c489b84e0f86ff8135a34548b854f9a
Ernesto Hinojosa <eh@ernesto.one>
2021-02-01 12:22:43 +0100

    add content to 404 page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+
+<!-- 404 - for now -->
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        Page Not Found
+      </div>
+    </div>
+  </div>
+<!-- /404 - for now -->
+
+<?php get_footer(); ?>
